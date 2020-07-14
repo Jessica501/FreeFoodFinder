97531fc4c07599f4373b0e7a3fd34491b9be3e45
Jessica Xu <jyxu@fb.com>
2020-07-14 12:12:02 -0400

    Add log in and sign up functionality and log in persistence

--- app/src/main/java/com/example/freefood/LoginActivity.java
+++ app/src/main/java/com/example/freefood/LoginActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.freefood.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity();
+        }
 
         binding.tvNoAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +36,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = String.valueOf(binding.etUsername.getText());
+                String password = String.valueOf(binding.etPassword.getText());
+                logInUser(username, password);
+            }
+        });
 
+
+    }
+
+    private void logInUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.i(TAG, "Successfully logged in");
+                    goToMainActivity();
+                } else if (e.getCode() == ParseException.USERNAME_MISSING) {
+                    Toast.makeText(LoginActivity.this, "Username cannot be empty.", Toast.LENGTH_SHORT).show();
+                } else if (e.getCode() == ParseException.PASSWORD_MISSING) {
+                    Toast.makeText(LoginActivity.this, "Password cannot be empty.", Toast.LENGTH_SHORT).show();
+                } else if (e.getCode() == ParseException.OBJECT_NOT_FOUND) {
+                    Toast.makeText(LoginActivity.this, "Invalid username/password.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error logging in. Check log.", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error logging in" + e.getCode(), e);
+                }
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/freefood/SignupActivity.java
+++ app/src/main/java/com/example/freefood/SignupActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.freefood.databinding.ActivitySignupBinding;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends AppCompatActivity {
 
     ActivitySignupBinding binding;
+    public static final String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,41 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(i);
+            }
+        });
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = String.valueOf(binding.etUsername.getText());
+                String password = String.valueOf(binding.etPassword.getText());
+                String name = String.valueOf(binding.etName.getText());
+                signUpUser(username, password, name);
+
+
+            }
+        });
+    }
+
+    private void signUpUser(String username, String password, String name) {
+        ParseUser parseUser = new ParseUser();
+        parseUser.setUsername(username);
+        parseUser.setPassword(password);
+        parseUser.put("name", name);
+        parseUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.i(TAG, "Successfully signed up");
+                    Intent i = new Intent(SignupActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } else if (e.getCode() == ParseException.USERNAME_TAKEN) {
+                    Toast.makeText(SignupActivity.this, "Username taken. Try again!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SignupActivity.this, "Error signing up. Check log.", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error signing up" + e.getCode(), e);
+                }
             }
         });
     }
