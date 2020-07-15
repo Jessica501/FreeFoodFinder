5d84115d2fdfe8d2ed393dccd59e33d99c614c22
Jessica Xu <jyxu@fb.com>
2020-07-15 11:53:43 -0400

    Add functionality to choose image from gallery

--- app/src/main/java/com/example/freefood/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/freefood/fragments/ComposeFragment.java
@@ -1,9 +1,12 @@
 package com.example.freefood.fragments;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -24,24 +27,32 @@ import com.example.freefood.CreateDetailActivity;
 import com.example.freefood.PostDetailActivity;
 import com.example.freefood.R;
 import com.example.freefood.databinding.FragmentComposeBinding;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 
 import static android.app.Activity.RESULT_OK;
 
-
+@RuntimePermissions
 public class ComposeFragment extends Fragment {
 
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
-    public static final String APP_TAG = "FreeFood";
+    public static final int PICK_PHOTO_CODE = 2;
+    public static final String TAG = "ComposeFragment";
 
     FragmentComposeBinding binding;
     public String photoFileName = "photo.jpg";
-    File photoFile;
-    ParseFile parseFile;
+    public File photoFile;
+    public byte[] photoBytes;
+    public ParseFile parseFile;
 
 
     public ComposeFragment() {
@@ -66,19 +77,28 @@ public class ComposeFragment extends Fragment {
             }
         });
 
+        binding.btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickPhoto(view);
+            }
+        });
+
         binding.btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final ParseFile image = new ParseFile(photoFile);
-                image.saveInBackground(new SaveCallback() {
+                parseFile.saveInBackground(new SaveCallback() {
                     @Override
                     public void done(ParseException e) {
+                        if (e != null) {
+                            Log.e(TAG, "Error saving image file to Parse", e);
+                            return;
+                        }
                         Intent i = new Intent(getContext(), PostDetailActivity.class);
-                        i.putExtra("image", image);
+                        i.putExtra("image", parseFile);
                         startActivity(i);
                     }
                 });
-
             }
         });
     }
@@ -100,19 +120,19 @@ public class ComposeFragment extends Fragment {
             // Start the image capture intent to take photo
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
-
     }
 
-    // Returns the File for a photo stored on disk given the fileName
+
+    // Returns the File for a photo stored on disk given the fileName (for using camera)
     private File getPhotoFileUri(String fileName) {
         // Get safe storage directory for photos
         // Use `getExternalFilesDir` on Context to access package-specific directories.
         // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(APP_TAG, "failed to create directory");
+            Log.d(TAG, "failed to create directory");
         }
 
         // Return the file target for the photo based on filename
@@ -121,18 +141,70 @@ public class ComposeFragment extends Fragment {
         return file;
     }
 
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    public void onPickPhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    // returns Bitmap from Uri for image selected from gallery
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if (Build.VERSION.SDK_INT > 27) {
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getContext().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image", e);
+        }
+        return image;
+    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Result from camera
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // by this point we have the camera photo on disk
                 Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // create parseFile
+                parseFile = new ParseFile(photoFile);
                 // Load the taken image into a preview
                 binding.ivImage.setImageBitmap(takenImage);
                 binding.btnContinue.setVisibility(View.VISIBLE);
             } else { // Result was a failure
                 Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
+        }
+        // Result from gallery
+        else if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+
+            // convert Bitmap to byte[], then create parseFile
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            selectedImage.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            photoBytes = stream.toByteArray();
+            parseFile = new ParseFile(photoBytes);
+
+            // Load the selected image into a preview
+            binding.ivImage.setImageBitmap(selectedImage);
+            binding.btnContinue.setVisibility(View.VISIBLE);
         }
     }
 }
