bcc4dc3a2d1f539141cdee86fa65faf2490f23aa
Jessica Xu <jyxu@fb.com>
2020-08-05 11:47:17 -0400

    Setup Firebase service

--- app/src/main/java/com/example/freefood/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/freefood/MyFirebaseMessagingService.java
@@ -1,16 +1,90 @@
 package com.example.freefood;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
-public class MyFirebaseMessagingService extends Service {
-    public MyFirebaseMessagingService() {
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "mFCMService";
+    private final String ADMIN_CHANNEL_ID ="admin_channel";
+    private static final String SUBSCRIBE_TO = "userABC";
+
+    @Override
+    public void onNewToken (String token) {
+        // Here you can include more complex logic for users to only be subscribed to certain topics
+        // For example, creating a container in your database for topics they are subscribed to,
+        // and then subscribing them to all of those topics upon app start
+        FirebaseMessaging.getInstance().subscribeToTopic(SUBSCRIBE_TO);
+        Log.i(TAG, "onTokenRefresh completed with token: " + token);
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        final Intent intent = new Intent(this, MainActivity.class);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        int notificationID = new Random().nextInt(3000);
+
+        /*
+          Apps targeting SDK 26 or above (Android O) must implement notification channels and add its notifications to at least one of them. Therefore, confirm if version is Oreo or higher, then setup notification channel
+        */
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels(notificationManager);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this , 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        // You can change the notification icon/picture here
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_baseline_fastfood_24);
+
+        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_fastfood_24)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("message"))
+                .setAutoCancel(true)
+                .setSound(notificationSoundUri)
+                .setContentIntent(pendingIntent);
+
+        //Set notification color to match your app color template
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {notificationBuilder.setColor(getResources().getColor(R.color.primaryDarkColor));
+        }
+        notificationManager.notify(notificationID, notificationBuilder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(NotificationManager notificationManager) {
+        CharSequence adminChannelName = "New notification";
+        String adminChannelDescription = "Device to device notification";
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
     }
 }
